055ab705c3f1f37560e6cdba0842e7fb5a765f4f
ParkJongHo <pjh1030pjh.com@gmail.com>
2025-03-29 18:03:32 +0900

    data

--- src/components/App/data.js
+++ src/components/App/data.js
@@ -1,4 +1,4 @@
-export const cityMap = {
+const cityMap = {
   서울: "Seoul",
   부산: "Busan",
   대구: "Daegu",
@@ -6,8 +6,20 @@ export const cityMap = {
   광주: "Gwangju",
   대전: "Daejeon",
   울산: "Ulsan",
-  용인: "Yongin",
   수원: "Suwon",
+  용인: "Yongin",
   청주: "Cheongju",
+  전주: "Jeonju",
+  안산: "Ansan",
+  평택: "Pyeongtaek",
   천안: "Cheonan",
+  김해: "Gimhae",
+  포항: "Pohang",
+  창원: "Changwon",
+  경주: "Gyeongju",
+  춘천: "Chuncheon",
+  원주: "Wonju",
+  강릉: "Gangneung",
+  여수: "Yeosu",
+  순천: "Suncheon",
 };
